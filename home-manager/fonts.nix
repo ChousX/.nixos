8041fa534cd7377ff9f8a52ece03ec89a56ddf58
ChousX <gmanenergy@gmail.com>
2024-07-02 15:29:34 -0400

    still trying to get the icons working -_-

--- home-manager/fonts.nix
+++ home-manager/fonts.nix
@@ -1,15 +1,16 @@
 {pkgs, ...} : {
   fonts.fontconfig.enable = true;
   home.packages = with pkgs; [
-    nerdfonts
-    noto-fonts
-    noto-fonts-cjk
-    noto-fonts-emoji
-    liberation_ttf
     fira-code
     fira-code-symbols
+    font-awesome
+    liberation_ttf
     mplus-outline-fonts.githubRelease
-    dina-font
+    nerdfonts
+    noto-fonts
+    noto-fonts-emoji
     proggyfonts
+
   ];
 }
+        
