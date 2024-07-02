ecf742a4e409f2b106445ed38384c668058aae56
ChousX <gmanenergy@gmail.com>
2024-07-02 13:30:09 -0400

    got hyperland to find my config

--- home-manager/hyperland.nix
+++ home-manager/hyperland.nix
@@ -1,21 +1,24 @@
-{...}: {
-  # Hyprland Config
+{pkgs, ...}: {
+  xdg.portal = {
+    extraPortals = [ pkgs.inputs.hyprland.xdg-desktop-portal-hyprland ];
+    configPackages = [ pkgs.inputs.hyprland.hyprland ];
+  };
+
+
   wayland.windowManager.hyprland = {
-    systemd.variables = ["--all"];
+    enable = true;
+    xwayland.enable = true;
     settings = {
-    "$mod" = "SUPER";
-    bind = [
-      "$mod, E, exec, emacs"
-      "$mod, R, exec, rofi -show run"
-      "$mod, F, exec, firefox"
-      "$mod, Return, exec, alacritty"
-      "$mod SHIFT, Q, killactive"
-      "$mod SHIFT, Delete, exec, hyprctl dispatch exit"
-      "$mod, A, exec, alacritty"
-      #", Print, exec, "
-    ] ++
-    (
-      builtins.concatLists (builtins.genList (
+      "monitor" = ",preferred,auto,auto";
+      "$mod" = "SUPER";
+      "$term" = "alacritty";
+
+      bind = [
+        "$mod, Return, exec, $term"
+        "$mod SHIFT, Q, killactive"
+        "$mod, F, exec, firefox"
+      ] ++ (
+        builtins.concatLists (builtins.genList (
           x: let
             ws = let
                 c = (x + 1) / 10;
@@ -25,11 +28,10 @@
             "$mod, ${ws}, workspace, ${toString (x + 1)}"
             "$mod SHIFT, ${ws}, movetoworkspace, ${toString(x + 1)}"
             ]
-        )
+          )
           10
         )
       );
     };
   };
-  
 }
