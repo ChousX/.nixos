5ff6a626d64dc74569426c60128d67792b99c50c
ChousX <gmanenergy@gmail.com>
2024-07-07 16:04:14 -0400

    ?

--- home-manager/fonts.nix
+++ home-manager/fonts.nix
@@ -1,15 +1,16 @@
 {pkgs, ...} : {
-  fonts.fontconfig.enable = true;
-  home.packages = with pkgs; [
+  #fonts.fontconfig.enable = true;
+
+  #home.packages = with pkgs; [
     nerdfonts
-    noto-fonts
-    noto-fonts-cjk
-    noto-fonts-emoji
-    liberation_ttf
-    fira-code
-    fira-code-symbols
-    mplus-outline-fonts.githubRelease
-    dina-font
-    proggyfonts
-  ];
+    #noto-fonts
+    #noto-fonts-cjk
+    #noto-fonts-emoji
+    #liberation_ttf
+    #fira-code
+    #fira-code-symbols
+    #mplus-outline-fonts.githubRelease
+    #dina-font
+    #proggyfonts
+  #];
 }
