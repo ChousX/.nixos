f0d9d47e62c51f62cf2af7cdde6ad853199d9698
ChousX <gmanenergy@gmail.com>
2024-07-08 07:51:40 -0400

    moving to kde 6

--- nixos/kde.nix
+++ nixos/kde.nix
@@ -4,22 +4,19 @@
     ./sddm.nix
   ];
 
-  services.xserver.desktopManager.plasma5.enable = true;
+  #services.xserver.desktopManager.plasma5.enable = true;
   # KDE Plasma 6 is now available on unstable
-  #services.desktopManager.plasma6.enable = true;
+  services.desktopManager.plasma6.enable = true;
 
-  #environment.plasma6.excludePackages = with pkgs.kdePackages; [
-    #plasma-browser-integration
-    #konsole
-    #oxygen
-  #];
+  environment.plasma6.excludePackages = with pkgs.kdePackages; [
+    plasma-browser-integration
+    konsole
+    oxygen
+  ];
 
   #environment.systemPackages = with pkgs.kdePackages; [];
 
-programs.kdeconnect = { 
-  enable = true;
-   
-};
-
-
+  programs.kdeconnect = { 
+    enable = true;
+  };
 }
