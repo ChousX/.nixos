142d92ab93b64262f71ccd7257cc61aea686e117
ChousX <gmanenergy@gmail.com>
2024-09-11 11:44:13 -0400

    messed around with alacrity settings

--- home-manager/alacritty.nix
+++ home-manager/alacritty.nix
@@ -1,12 +1,12 @@
 { ... } : {
   programs.alacritty = {
     enable = true;
-    settings = {
-      font = {
-        normal.family = "RobotoMono Nerd Font";
-        size = "16.0";
-      };
-      cursor.style.shape = "Beam";
-    };
+    #settings = {
+      #font = {
+        #normal.family = "RobotoMono Nerd Font";
+        #size = "16.0";
+      #};
+      #cursor.style.shape = "Beam";
+    #};
   };
 }
