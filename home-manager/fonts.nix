34698c1a2a449ae33a047111e4669bf34142044b
ChousX <gmanenergy@gmail.com>
2024-09-20 08:11:21 -0400

    removed all fonts other than nurdfonts opendyslexic

--- home-manager/fonts.nix
+++ home-manager/fonts.nix
@@ -2,7 +2,7 @@
 
 fonts.fontconfig.enable = true;
 home.packages = with pkgs; [
-  (nerdfonts.override { fonts = ["OpenDyslexic"]})
+  (nerdfonts.override { fonts = ["OpenDyslexic"];})
   ];
 }
         
